af8d878d4438bd6da8c24b4e7d85d37cf9ed1395
sarathejohn <srteon13@gmail.com>
2021-07-31 22:50:05 +0530

    july31

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+
+import cors from  'cors';
+import express from 'express';
+import consola  from 'consola';
+import mongoose  from 'mongoose';
+// import { json } from 'body-parse';
+
+
+//import application constants
+import {DB,PORT} from "./constants"
+
+//Initialize express application
+const app = express();
+
+
+const main =async ()=>{
+    try {
+        //connect with database
+       await mongoose.connect(DB,{
+            useNewUrlParser:true,
+            useFindAndModify:false,
+            useUnifiedTopology:true
+        });
+        consola.success("DATABASE CONNECTED...");
+
+        //start application listerning for request on  server
+        app.listern(PORT,()=>consola.success(`server is starting at port ${PORT}`));
+    } catch (err) {
+        
+    }
+}
+main();
